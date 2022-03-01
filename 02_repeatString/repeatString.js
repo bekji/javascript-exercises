9aef6096de477e620dc22aea1c098a9f1151bd61
samandar__e <san.panjiyev@yandex.ru>
2022-03-01 13:10:52 +0500

    first solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    let res = ''
+    if(str !== '') {
+        if(n === -1) {
+            str = 'ERROR';
+            return str
+        } else if(n === 0) {
+            str = '';
+            return str
+        } else {
+            for(let i = 1; i <= n; i++) {
+                res = res.concat(str)
+            }
+            return res
+        }
+    } else {
+        return str;
+    }
 };
 
 // Do not edit below this line

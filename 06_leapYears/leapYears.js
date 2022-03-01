03da7d78080b6ab114105f9675321b937b68af20
samandar__e <san.panjiyev@yandex.ru>
2022-03-01 15:56:10 +0500

    conversion added

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,15 @@
 const leapYears = function(year) {
-    if(year % 100 === 0) {
-        if(year % 400 === 0) {
-            return true;
-        }
-        return false;
-    }
-    if(year % 4 === 0) {
-        return true;
-    }
-    return false;
+    // if(year % 100 === 0) {
+    //     if(year % 400 === 0) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    // if(year % 4 === 0) {
+    //     return true;
+    // }
+    // return false;
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const res = (temp - 32) * 5/9;
+  return res === 0 ? 0 : Number(res).toFixed(1)
 };
 
 const ctof = function() {
-
+  const res = (temp * 9/5) + 32;
+  return res === 32 ? 32 : Number(res).toFixed(1)
 };
 
 // Do not edit below this line

1dbed999d36f92f8ccbff0f012b6da012ee2f6a7
samandar__e <san.panjiyev@yandex.ru>
2022-03-01 17:12:37 +0500

    round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,9 @@
 const ftoc = function(temp) {
-  const res = (temp - 32) * 5/9;
-  return res === 0 ? 0 : Number(res).toFixed(1)
+  return Math.round((temp - 32) * 5/9 * 10) / 10;
 };
 
 const ctof = function() {
-  const res = (temp * 9/5) + 32;
-  return res === 32 ? 32 : Number(res).toFixed(1)
+  return Math.round((temp * 9/5) + 32 * 10 ) / 10;
 };
 
 // Do not edit below this line

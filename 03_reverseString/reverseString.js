946705fe64a5417178ccd17b45118cd193c14c68
samandar__e <san.panjiyev@yandex.ru>
2022-03-01 13:26:01 +0500

    second

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let splitWord = word.split("");
+    let reversed = splitWord.reverse();
+    let reverseJoint = reversed.join("");
+    return reverseJoint;
 };
 
 // Do not edit below this line
